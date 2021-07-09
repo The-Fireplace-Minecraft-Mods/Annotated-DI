25752e9e021619868c29456541524f4b6986ec7b
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-09 17:40:43 -0500

    Remove Reflections and start on a new SourceSet for an annotation processor

--- src/main/java/dev/the_fireplace/annotateddi/api/di/Implementation.java
+++ src/main/java/dev/the_fireplace/annotateddi/api/di/Implementation.java
@@ -1,10 +1,6 @@
 package dev.the_fireplace.annotateddi.api.di;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
 public @interface Implementation {
-    Class<?>[] value() default {Object.class};
+    Class<?>[] value() default Object.class;
     String name() default "";
 }
