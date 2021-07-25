ffd390ea2c4871830adad5f7bd6343d4e79ec925
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-25 12:14:58 -0500

    Fix crash when loading dedicated server

--- src/main/java/dev/the_fireplace/annotateddi/impl/AnnotatedDIServer.java
+++ src/main/java/dev/the_fireplace/annotateddi/impl/AnnotatedDIServer.java
@@ -3,14 +3,15 @@ package dev.the_fireplace.annotateddi.impl;
 import com.google.inject.Injector;
 import dev.the_fireplace.annotateddi.api.DIContainer;
 import dev.the_fireplace.annotateddi.api.entrypoints.DedicatedServerDIModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 
 @Environment(EnvType.SERVER)
-public final class AnnotatedDIServer implements DedicatedServerDIModInitializer {
+public final class AnnotatedDIServer implements DedicatedServerModInitializer {
     @Override
-    public void onInitializeServer(Injector diContainer) {
+    public void onInitializeServer() {
         Injector container = DIContainer.get();
         FabricLoader.getInstance().getEntrypointContainers("di-server", DedicatedServerDIModInitializer.class)
             .forEach((entrypoint) -> entrypoint.getEntrypoint().onInitializeServer(container));
