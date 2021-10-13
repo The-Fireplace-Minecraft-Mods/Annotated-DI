7e2df5807105d24af6417ace8ce62df22ba8eec1
The_Fireplace <the.f1repl4ce@gmail.com>
2021-10-12 20:22:15 -0500

    Throw an error when a bad annotation is found, hopefully stopping the build.

---
+++ src/annotationprocessor/java/dev/the_fireplace/annotateddi/processor/ImplementationProcessingError.java
@@ -0,0 +1,8 @@
+package dev.the_fireplace.annotateddi.processor;
+
+public class ImplementationProcessingError extends Error {
+    @Override
+    public String getMessage() {
+        return "Errors occurred when processing @Implementation annotated classes. See logs for more details.";
+    }
+}
