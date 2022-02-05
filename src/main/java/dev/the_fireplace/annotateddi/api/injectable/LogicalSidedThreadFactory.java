ed730d660313289ab7d0aa851ed1e7bcbd8f30e7
The_Fireplace <the.f1repl4ce@gmail.com>
2022-02-05 14:37:57 -0600

    Revert logical proxy changes
    Major flaw with the logical proxy idea is that we get the client implementation when the main entrypoint is initializing on a client. In most cases we need the common implementation there, so leaving this in could lead to unexpected problems.

--- src/main/java/dev/the_fireplace/annotateddi/api/injectable/LogicalSidedThreadFactory.java
+++
@@ -1,9 +0,0 @@
-package dev.the_fireplace.annotateddi.api.injectable;
-
-public interface LogicalSidedThreadFactory
-{
-    /**
-     * Create a new thread with side data attached. Don't forget to start it!
-     */
-    Thread createNewThread(Runnable runnable);
-}
