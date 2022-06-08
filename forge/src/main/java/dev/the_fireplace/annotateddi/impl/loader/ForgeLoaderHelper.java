59124374ece04ec794acfeb131c3f7d271f1b847
The_Fireplace <git@the-fireplace.dev>
2022-06-07 21:44:47 -0500

    Backport to Java 8

--- forge/src/main/java/dev/the_fireplace/annotateddi/impl/loader/ForgeLoaderHelper.java
+++ forge/src/main/java/dev/the_fireplace/annotateddi/impl/loader/ForgeLoaderHelper.java
@@ -5,6 +5,8 @@ import dev.the_fireplace.annotateddi.impl.domain.loader.LoaderHelper;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
+import net.minecraftforge.forgespi.language.IModFileInfo;
 import net.minecraftforge.forgespi.language.IModInfo;
 
 import java.nio.file.Files;
@@ -33,7 +35,8 @@ public final class ForgeLoaderHelper implements LoaderHelper
     @Override
     public Collection<String> getDependencies(String modId) {
         Optional<? extends ModContainer> modContainer = ModList.get().getModContainerById(modId);
-        if (modContainer.isEmpty()) {
+        //noinspection OptionalIsPresent
+        if (!modContainer.isPresent()) {
             return Collections.emptySet();
         }
         return modContainer.get().getModInfo().getDependencies().stream()
@@ -48,22 +51,28 @@ public final class ForgeLoaderHelper implements LoaderHelper
 
     @Override
     public boolean isOnEnvironment(String environment) {
-        return switch (environment.toLowerCase(Locale.ROOT)) {
-            case "client" -> FMLEnvironment.dist.isClient();
-            case "server" -> FMLEnvironment.dist.isDedicatedServer();
-            default -> throw new IllegalStateException("Unknown environment: " + environment);
-        };
+        switch (environment.toLowerCase(Locale.ROOT)) {
+            case "client":
+                return FMLEnvironment.dist.isClient();
+            case "server":
+                return FMLEnvironment.dist.isDedicatedServer();
+            default:
+                throw new IllegalStateException("Unknown environment: " + environment);
+        }
     }
 
     @Override
     public Optional<Path> findDiConfigPath(String modId) {
         Optional<? extends ModContainer> modContainer = ModList.get().getModContainerById(modId);
-        if (modContainer.isEmpty()) {
+        if (!modContainer.isPresent()) {
             return Optional.empty();
         }
-        Path resource = modContainer.get().getModInfo().getOwningFile().getFile().findResource(DI_CONFIG_FILE_NAME);
-        if (Files.exists(resource)) {
-            return Optional.of(resource);
+        IModFileInfo owningFile = modContainer.get().getModInfo().getOwningFile();
+        if (owningFile instanceof ModFileInfo) {
+            Path resource = ((ModFileInfo)owningFile).getFile().findResource(DI_CONFIG_FILE_NAME);
+            if (Files.exists(resource)) {
+                return Optional.of(resource);
+            }
         }
         return Optional.empty();
     }
