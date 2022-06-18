3466d87ab6ab90fdb8426b1c5baa89d914d1205b
The_Fireplace <git@the-fireplace.dev>
2022-06-18 18:43:14 -0500

    Do not initialize other mods' Injectors from Fabric entrypoints
    Other mods should initialize their own Injectors, because we want them to be the ones shown in the crash report if their DI configs are broken.

--- fabric/src/main/java/dev/the_fireplace/annotateddi/api/entrypoints/ClientDIModInitializer.java
+++ fabric/src/main/java/dev/the_fireplace/annotateddi/api/entrypoints/ClientDIModInitializer.java
@@ -1,11 +1,10 @@
 package dev.the_fireplace.annotateddi.api.entrypoints;
 
-import com.google.inject.Injector;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
 public interface ClientDIModInitializer
 {
-    void onInitializeClient(Injector diContainer);
+    void onInitializeClient();
 }
--- fabric/src/main/java/dev/the_fireplace/annotateddi/api/entrypoints/DIModInitializer.java
+++ fabric/src/main/java/dev/the_fireplace/annotateddi/api/entrypoints/DIModInitializer.java
@@ -1,8 +1,6 @@
 package dev.the_fireplace.annotateddi.api.entrypoints;
 
-import com.google.inject.Injector;
-
 public interface DIModInitializer
 {
-    void onInitialize(Injector diContainer);
+    void onInitialize();
 }
