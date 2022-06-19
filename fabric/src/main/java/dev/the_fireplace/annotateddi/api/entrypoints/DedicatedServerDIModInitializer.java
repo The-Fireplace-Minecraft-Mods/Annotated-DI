c5baa6bf332f2379f5e4fc94815cd28d1d73b8e3
The_Fireplace <git@the-fireplace.dev>
2022-06-18 19:16:13 -0500

    Remove custom Client/Server/Main entrypoint interfaces
    Since we no longer want to pass the injector through these entrypoints, we can use Fabric's built in entrypoints instead. Allow registering to di-* so mods can ensure they are loaded after custom modules

--- fabric/src/main/java/dev/the_fireplace/annotateddi/api/entrypoints/DedicatedServerDIModInitializer.java
+++
@@ -1,6 +0,0 @@
-package dev.the_fireplace.annotateddi.api.entrypoints;
-
-public interface DedicatedServerDIModInitializer
-{
-    void onInitializeServer();
-}
--- fabric/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Main.java
+++ fabric/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Main.java
@@ -1,6 +1,5 @@
 package dev.the_fireplace.annotateddi.impl.entrypoint;
 
-import dev.the_fireplace.annotateddi.api.entrypoints.DIModInitializer;
 import dev.the_fireplace.annotateddi.impl.di.FabricInjectorSetup;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -12,7 +11,7 @@ public final class Main implements ModInitializer
         FabricInjectorSetup.init();
         FabricLoader.getInstance().getEntrypointContainers(
             "di-main",
-            DIModInitializer.class
+            ModInitializer.class
         ).forEach(entrypoint -> entrypoint.getEntrypoint().onInitialize());
     }
 }
