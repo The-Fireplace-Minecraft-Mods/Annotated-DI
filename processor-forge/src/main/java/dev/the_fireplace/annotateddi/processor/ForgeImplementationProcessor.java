9bf9edecacdef0796b7ba505440a8fe03e4fe962
The_Fireplace <git@the-fireplace.dev>
2024-07-06 12:51:18 -0500

    Pull duplicate annotation data into the parent class

--- processor-forge/src/main/java/dev/the_fireplace/annotateddi/processor/ForgeImplementationProcessor.java
+++ processor-forge/src/main/java/dev/the_fireplace/annotateddi/processor/ForgeImplementationProcessor.java
@@ -3,13 +3,8 @@ package dev.the_fireplace.annotateddi.processor;
 import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.Processor;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 
-@SupportedAnnotationTypes("dev.the_fireplace.annotateddi.api.di.Implementation")
-@SupportedSourceVersion(SourceVersion.RELEASE_16)
 @AutoService(Processor.class)
 public final class ForgeImplementationProcessor extends ImplementationProcessor
 {
