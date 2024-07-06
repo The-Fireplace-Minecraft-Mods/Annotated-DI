d168233b1dffdcd4145825240e144f821b36c1bd
The_Fireplace <git@the-fireplace.dev>
2024-07-05 19:16:17 -0500

    Port to 1.20.6

--- forge/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Forge.java
+++ forge/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Forge.java
@@ -3,13 +3,12 @@ package dev.the_fireplace.annotateddi.impl.entrypoint;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod("annotateddi")
 public final class Forge
 {
     public Forge() {
         // Register as optional on both sides
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
