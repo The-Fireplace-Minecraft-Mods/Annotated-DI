5e42c1941444417f06cd2f1bcf396c40a0624939
The_Fireplace <git@the-fireplace.dev>
2024-07-07 10:14:33 -0500

    Optimize injector speed in production by providing the correct Stage

--- forge/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Forge.java
+++ forge/src/main/java/dev/the_fireplace/annotateddi/impl/entrypoint/Forge.java
@@ -1,14 +1,17 @@
 package dev.the_fireplace.annotateddi.impl.entrypoint;
 
+import dev.the_fireplace.annotateddi.impl.injector.AnnotatedDIInjectorLoader;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod("annotateddi")
 public final class Forge
 {
     public Forge() {
+        AnnotatedDIInjectorLoader.setDevelopmentEnvironment(!FMLEnvironment.production);
         // Register as optional on both sides
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
